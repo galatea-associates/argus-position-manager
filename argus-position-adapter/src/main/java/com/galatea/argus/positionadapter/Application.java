2e8b5f72d79ca0a097323e9b5624cfe34571d731
Edward Gerrard <gerrard@galatea-associates.com>
2019-05-29 11:35:21 +0100

    "First Commit"

---
+++ argus-position-adapter/src/main/java/com/galatea/argus/positionadapter/Application.java
@@ -0,0 +1,13 @@
+package com.galatea.argus.positionadapter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
